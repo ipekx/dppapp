61c96d7050c56eb14113f10110b56fd81f3fc839
ipekx <uhfsd@student.kit.edu>
2024-06-17 12:29:34 +0200

    grid view collections

---
+++ app/src/main/java/com/example/digitalproductapp/adapter/GridRecyclerViewAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.digitalproductapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.digitalproductapp.R
+
+class GridRecyclerViewAdapter : RecyclerView.Adapter<GridRecyclerViewAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.collection, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return 25
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
